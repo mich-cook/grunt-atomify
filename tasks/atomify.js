783e49e85a7ad814bbd3f6a8afb63dd8bfa7a279
mich-cook <mcook@sjc-dev-usrv118.corp.coupons.com>
2014-06-23 15:06:15 -0700

    initial checkin

---
+++ tasks/atomify.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var fs = require('fs');
+var atomify = require('atomify');
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('atomify', 'Atomify all the stuff grunt says to.', function() {
+
+        var options = this.options();
+        var done = this.async();
+
+        var cssConfig = options.cssConfig;
+        var jsConfig = options.jsConfig;
+        var atomifyConfig = {};
+
+        var lastSlash, path,
+            directory = '', directories;
+
+        function mkdirIfNeeded(outputFile)  {
+            if(outputFile.lastIndexOf('/') > 0)  {
+                // mkdir the output if it doesn't exist
+                lastSlash = outputFile.lastIndexOf('/');
+                path = outputFile.slice(0, lastSlash);
+
+                if(fs.existsSync(path) === false)  {
+                    directories = path.split('/').forEach(function(element, index, array)  {
+                        directory += element + '/';
+                        fs.mkdir(directory);
+                    });
+                }
+            }
+
+        }
+
+        if(jsConfig !== undefined)  {
+            // if there's a directory in the output, we have to make it
+            mkdirIfNeeded(jsConfig.output);
+            // add the JS config to the overall config that we're passing to atomify
+            atomifyConfig.js = jsConfig;
+        }
+
+        if(cssConfig !== undefined)  {
+            // if there's a directory in the output, we have to make it
+            mkdirIfNeeded(cssConfig.output);
+            // add the CSS config to the overall config that we're passing to atomify
+            atomifyConfig.css = cssConfig;
+        }
+
+        if((cssConfig !== undefined) || (jsConfig !== undefined))  {
+            atomify(atomifyConfig, function(error)  {
+                done();
+            });
+        }  else   {
+            done();  // nothing to do. done.
+        }
+
+  });
+
+};
